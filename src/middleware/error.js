23a9d1364e4f12b423203fda34e759224d368206
Adams Temidire <gr8temi@gmail.com>
2021-08-12 13:26:58 +0100

    fix issues

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -2,12 +2,12 @@ import isPromise from "../utils/isPromise";
 import handleError from "../utils/handleError";
 
 export default function errorMiddleware() {
-  return next => action => {
+  return (next) => (action) => {
     let result = next(action);
     if (!isPromise(result)) {
       return action;
     }
-    return result.catch(error => {
+    return result.catch((error) => {
       handleError(error);
       return error;
     });
